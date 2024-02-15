8a43f6fa0fbafe72ea79fca79446c58969588e38
Fish <sm29729443@gmail.com>
2024-02-15 19:19:48 +0800

    基本 project 配置初始化

---
+++ mall-backend/src/test/java/com/tong/mallbackend/MallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tong.mallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
