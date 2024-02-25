b6109c77a6c77a89c9262078dc7d87da4ee617b4
Fish <sm29729443@gmail.com>
2024-02-26 02:12:40 +0800

    OrderController小修改

--- mall-backend/src/main/java/com/tong/mallbackend/controller/CartController.java
+++ mall-backend/src/main/java/com/tong/mallbackend/controller/CartController.java
@@ -2,9 +2,15 @@ package com.tong.mallbackend.controller;
 
 import com.tong.mallbackend.dto.CartDetailRequest;
 import com.tong.mallbackend.dto.CartItem;
+import com.tong.mallbackend.dto.ErrorResponseBody;
 import com.tong.mallbackend.models.CartDetailEntity;
+import com.tong.mallbackend.models.CategoryEntity;
 import com.tong.mallbackend.models.ProductEntity;
 import com.tong.mallbackend.service.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,8 +32,22 @@ public class CartController {
     private CartService cartService;
 
     // 新增商品至購物車
+    @Operation(summary = "新增商品至購物車功能",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "商品放入購物車成功",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "400", description = "商品庫存不足",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+                    @ApiResponse(responseCode = "401", description = "尚未登入",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+                    @ApiResponse(responseCode = "404", description = "商品不存在",
+                            content = @Content(mediaType = "application/json"))}
+    )
     @PostMapping("/users/cart")
-    public ResponseEntity<?> addProductToCart(@RequestBody @Valid CartDetailRequest request,
+    public ResponseEntity<Void> addProductToCart(@RequestBody @Valid CartDetailRequest request,
                                               HttpSession session) {
         Integer cartId = (Integer) session.getAttribute("cartId");
         cartService.addProductToCart(request, cartId);
@@ -35,15 +55,39 @@ public class CartController {
     }
 
     //刪除整個商品至購物車
+    @Operation(summary = "刪除購物車裡的商品功能，不是更新數量，是直接將此商品移除",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "商品刪除成功",
+                            content = @Content(mediaType = "application/json")),
+                    @ApiResponse(responseCode = "401", description = "尚未登入",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class)))
+            }
+    )
     @DeleteMapping("/users/cart/{cartDetailId}")
-    public ResponseEntity<?> removeProductFromCart(@PathVariable Integer cartDetailId) {
+    public ResponseEntity<Void> removeProductFromCart(@PathVariable Integer cartDetailId) {
         cartService.removeProductFromCart(cartDetailId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //編輯購物車裡的商品數量
+    @Operation(summary = "編輯購物車裡的商品數量功能",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "商品數量編輯成功",
+                            content = @Content(mediaType = "application/json", schema = @Schema(type = "object", implementation = CartDetailEntity.class))),
+                    @ApiResponse(responseCode = "400", description = "參數錯誤或商品庫存不足",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+                    @ApiResponse(responseCode = "401", description = "尚未登入",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+                    @ApiResponse(responseCode = "404", description = "商品不存在或於購物車裡不存在",
+                            content = @Content(mediaType = "application/json"))}
+    )
     @PutMapping("/users/cart/cartDetail")
-    public ResponseEntity<?> updateProductQuantityFromCart(@RequestBody CartDetailRequest request,
+    public ResponseEntity<CartDetailEntity> updateProductQuantityFromCart(@RequestBody @Valid CartDetailRequest request,
                                                            HttpSession session) {
         Integer cartId = (Integer) session.getAttribute("cartId");
         CartDetailEntity cartDetailEntity = cartService.updateProductQuantityFromCart(request, cartId);
@@ -51,6 +95,13 @@ public class CartController {
     }
 
     //查看購物車
+    @Operation(summary = "查看購物車功能",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "查詢成功",
+                            content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = CartItem.class))),
+            }
+    )
     @GetMapping("/users/cart")
     public ResponseEntity<List<CartItem>> getProductListFromCart(HttpSession session) {
         Integer cartId = (Integer) session.getAttribute("cartId");
--- mall-backend/src/main/java/com/tong/mallbackend/controller/OrderController.java
+++ mall-backend/src/main/java/com/tong/mallbackend/controller/OrderController.java
@@ -2,10 +2,16 @@ package com.tong.mallbackend.controller;
 
 import com.tong.mallbackend.constants.OrderStatus;
 import com.tong.mallbackend.dao.OrderDao;
+import com.tong.mallbackend.dto.ErrorResponseBody;
 import com.tong.mallbackend.dto.OrderDetailRequest;
 import com.tong.mallbackend.dto.OrderItem;
+import com.tong.mallbackend.models.CartDetailEntity;
 import com.tong.mallbackend.models.OrderDetailEntity;
 import com.tong.mallbackend.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +31,13 @@ public class OrderController {
 
     // 新增訂單
     // 訂單建立前，一定會經過購物車確認購買商品，所以這裡設計成訂單的建立就只是從購物車表轉移
+    @Operation(summary = "新增訂單功能",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "訂單建立成功",
+                            content = @Content(mediaType = "application/json", schema = @Schema(type = "object", implementation = CartDetailEntity.class))),
+            }
+    )
     @PostMapping("/users/orders")
     public ResponseEntity<Void> createOrder(HttpSession session) {
         Integer cartId = (Integer) session.getAttribute("cartId");
@@ -35,6 +48,16 @@ public class OrderController {
 
     // 查詢訂單列表
     // 因為訂單的資料量小，先暫定不給查詢條件
+    @Operation(summary = "查詢會員的所有訂單",
+            // 設定 Swagger responseBody
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "查詢成功",
+                            content = @Content(mediaType = "application/json", schema = @Schema(type = "array", implementation = OrderItem.class))),
+                    @ApiResponse(responseCode = "401", description = "尚未登入",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+            }
+    )
     @GetMapping("/users/orders")
     public ResponseEntity<List<OrderItem>> getOrderItems(HttpSession session) {
         Integer userId = (Integer) session.getAttribute("userId");
@@ -42,12 +65,22 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderItemList);
     }
 
-    // 刪除訂單
-    @DeleteMapping("/users/orders/{orderId}")
-    public ResponseEntity<Void> deleteOrder(HttpSession session,
-                                            @PathVariable Integer orderId) {
-        Integer userId = (Integer) session.getAttribute("userId");
-        orderService.deleteOrder(orderId, userId);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+    // 刪除訂單，還沒想到應用的場景
+//    @Operation(summary = "刪除某筆訂單",
+//            // 設定 Swagger responseBody
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "刪除成功",
+//                            content = @Content(mediaType = "application/json")),
+//                    @ApiResponse(responseCode = "401", description = "尚未登入",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = ErrorResponseBody.class))),
+//            }
+//    )
+//    @DeleteMapping("/users/orders/{orderId}")
+//    public ResponseEntity<Void> deleteOrder(HttpSession session,
+//                                            @PathVariable Integer orderId) {
+//        Integer userId = (Integer) session.getAttribute("userId");
+//        orderService.deleteOrder(orderId, userId);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
 }
