372b22376c76e3663dc4a78772d9a4619bfc4936
Fish <sm29729443@gmail.com>
2024-03-20 23:23:06 +0800

    app p tra

--- mall-backend/src/main/java/com/tong/mallbackend/MallBackendApplication.java
+++ mall-backend/src/main/java/com/tong/mallbackend/MallBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MallBackendApplication {
+    //http://localhost:8080/swagger-ui/index.html Swagger
 
     public static void main(String[] args) {
         SpringApplication.run(MallBackendApplication.class, args);
